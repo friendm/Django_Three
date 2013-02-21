8b8639c24b13b0c624182c0a6dbdc8631a8cd722
Mike_linux <sodium56@gmail.com>
2013-02-20 20:44:56 -0500

    pub date hierarchy not working

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -4,13 +4,19 @@ from django.utils import timezone
 
 
 class Poll(models.Model):	
-	question = models.CharField(max_length=200)
-	pub_date = models.DateTimeField('date published')
+	
 	def __unicode__(self):
 		return self.question
-	def recently_published(self):
+	
+	def was_published_recently(self):
 		return self.pub_date>=timezone.now()-datetime.timedelta(days=1)
-
+	
+	was_published_recently.admin_order_field='pub_date'
+	was_published_recently.boolean=True
+	was_published_recently.short_description='Published Recently?'
+	question = models.CharField(max_length=200)
+	pub_date = models.DateTimeField('date published', auto_now_add=True)
+	
 class Choice(models.Model):
 	def __unicode__(self):
 		return self.choice
