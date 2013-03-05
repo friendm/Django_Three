0e3c85fe3392af97cde844300113bca5e4ba7cc7
Michael Friend <michaeljfriend@gmail.com>
2013-03-04 18:56:44 -0600

    [done with url tutorial,need to create 400 and 500 pages in index of template]

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -1,7 +1,14 @@
-
 from django.conf.urls import patterns, url
-
 from polls import views
+#ex: /polls/
+urlpatterns = patterns('',
+url(r'^$', views.index, name ='index'),
+url(r'^(?P<poll_id>\d+)/$',views.detail, name = 'detail'),
+url(r'^(?P<poll_id>\d+)/results/$',views.results, name = 'results'),
+url(r'^(?P<poll_id>\d+)/vote/$',views.vote, name = 'vote'),
 
-urlpatterns = patterns('', url(r'^$', views.index, name='index')
+
 )
+
+
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,12 +1,26 @@
 #Create your views here.
 
 from django.http import HttpResponse
+from django.http import Http404
+from django.shortcuts import render,get_object_or_404
+from django.template import Context,loader
+from polls.models import Poll
+
 
 def index(request):
-	return HttpResponse("hello world, you are at the poll index")
+	latest_poll_list=Poll.objects.order_by('-pub_date')[:2]
+	template= loader.get_template('polls/first_template.html')
+	context = Context({ 'latest_poll_list':latest_poll_list,
+	})
+	return HttpResponse(template.render(context))
 
 def detail(request,poll_id):
-	return HttpResponse("you are looking at poll %s.",% poll_id)
+	poll=get_object_or_404(Poll,pk=poll_id)
+	return render(request,'polls/detail.html', {'poll':poll})
 
 def vote(request,poll_id):
-	return HttpRespnse("you'ee voting in poll %s", % poll_id)
+	return HttpResponse("you're voting in poll %s" % poll_id)
+def results(request,poll_id):
+	return HttpResponse("you're looking at the results of poll %s" % poll_id)
+
+
