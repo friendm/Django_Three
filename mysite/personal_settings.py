e522eb7aee810bd5f9b0e690bf56cc7bd270f9b1
Mike_linux <sodium56@gmail.com>
2013-03-07 21:31:17 -0500

    [need to modify settings file so that sql lite datbase is always in the same place]

---
+++ mysite/personal_settings.py
@@ -0,0 +1,4 @@
+sql_lite_database=test_database.db
+
+
+
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -20,13 +20,20 @@ def detail(request,poll_id):
 	return render(request,'polls/detail.html', {'poll':poll})
 
 def vote(request,poll_id):
+	#this assigns the opject to p based on the value that is passed via the web url
 	p=get_object_or_404(Poll,pk=poll_id)
 	try:
 		selected_choice=p.choice_set.get(pk=request.POST['choice'])
 	except(KeyError, Choice.DoesNotExist):
 	#redisplays the poll voting form
-return HttpResponse("you're voting in poll %s" % poll_id)
-
+		return render(request,"polls/vote.html",{'poll':p,'error_message':"You didn't select a choice",})
+	else:
+		selected_choice.votes+=1
+		selected_choice.save()
+		# Always return an HttpResponseRedirect after successfully dealing
+		# with POST data. This prevents data from being posted twice if a
+		# user hits the Back button.
+		return HttpResponseRedirect(reverse('polls:results', args=(p.id,)))
 
 def results(request,poll_id):
 	return HttpResponse("you're looking at the results of poll %s" % poll_id)
