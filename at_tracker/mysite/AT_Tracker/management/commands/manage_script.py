f6f0245fca8edc73e592a9f42e98f0e5973e9c26
Mike_linux <sodium56@gmail.com>
2013-03-23 19:02:59 -0400

    [named objects uniquley, writting methods to check if values are unique before inputting them into database, also need to write programs to read in CSV values]

---
+++ at_tracker/mysite/AT_Tracker/management/commands/manage_script.py
@@ -0,0 +1,38 @@
+from django.core.management.base import BaseCommand,CommandError
+from django.core.management import call_command
+import os
+import sys
+
+SETTINGS_PATH = os.path.abspath(__file__)
+for i in range(5):
+    SETTINGS_PATH = os.path.dirname(SETTINGS_PATH)
+sys.path.insert(0, os.path.join(SETTINGS_PATH, 'mysite'))
+
+print SETTINGS_PATH
+
+
+from personal_settings import BASE_PATH
+
+class Command(BaseCommand):
+	args='filename'
+	help='creates new sqlite database'
+	
+	def handle(self,*args,**options):
+		path = BASE_PATH
+		
+		if os.path.exists(path):
+			open(path,'w').close()
+		call_command('syncdb',interactive=False)
+			
+	
+		
+		
+		
+
+
+
+		
+		
+		
+	
+	
--- at_tracker/mysite/AT_Tracker/models.py
+++ at_tracker/mysite/AT_Tracker/models.py
@@ -3,38 +3,68 @@ from django.db import models
 class Schools(models.Model):
 	Name = models.CharField("Name of School", max_length = 50)
 	City = models.CharField("City", max_length = 50)
+		
+	def __unicode__(self): #this names the variable for the admin site
+		Unique =self.Name+self.City 
+		return Unique
+	def is_unique(self,Name,City):
+		Name_List = models.Schools.objects.values('Name').distinct()
+		City_List = models.Schools.objects.values('City').distinct()
+		
+		if Name_List.index(Name)==True & City_List.index(City):
+			return False
+		else:
+			return True
+			
+		
+			
 
 class Users(models.Model):
 	Username = models.CharField("Username", max_length = 50)
 	Password = models.CharField("Password", max_length = 50)
 	School = models.ForeignKey(Schools)
+	
+	def __unicode__(self): #this names the variable for the admin site
+		return self.Username
 
 class Grade(models.Model):
-		Grade_Level=models.IntegerField()
-		School = models.ForeignKey(Schools)
-	
+	Grade_Level = models.IntegerField()
+	School = models.ForeignKey(Schools)
+		
+	def __unicode__(self): #this names the variable for the admin site
+		Unique_Name = self.Grade_Level+self.School
+		return Unique_Name		
+				
 
 class Students(models.Model):
-	First_Name=models.CharField("First Name" , max_length = 50 )
-	Last_Name=models.CharField("Last Name" , max_length = 50 )
-	Middle_Name=models.CharField("Middle Name" , max_length = 50 )
+	First_Name = models.CharField("First Name" , max_length = 50 )
+	Last_Name = models.CharField("Last Name" , max_length = 50 )
+	Middle_Name = models.CharField("Middle Name" , max_length = 50 )
 	School = models.ForeignKey(Schools)
 	Grade = models.ForeignKey(Grade)
-
+		
+	def __unicode__(self): #this names the variable for the admin site
+		Unique_Name = self.First_Name+self.Last_Name+self.School+self.Grade
+		return Unique_Name		
+				
 
 class Teacher(models.Model):
-		First_Name=models.CharField("First Name" , max_length = 50 )
-		Last_Name=models.CharField("Last Name" , max_length = 50 )
-		Middle_Name=models.CharField("Middle Name" , max_length = 50 )
-		School = models.ForeignKey(Schools)
+	First_Name = models.CharField("First Name" , max_length = 50 )
+	Last_Name = models.CharField("Last Name" , max_length = 50 )
+	Middle_Name = models.CharField("Middle Name" , max_length = 50 )
+	School = models.ForeignKey(Schools)
 
-
+	def __unicode__(self): #this names the variable for the admin site
+		Unique_Name = self.First_Name+self.Last_Name+self.Middle_Name+self.School
+		return Unique_Name	
 
 class Classes(models.Model):
-		Name = models.CharField("Name of Class", max_length = 50)
-		Grade= models.ForeignKey(Grade)
-		School = models.ForeignKey(Schools)
-		Teachers = models.ManyToManyField(Teacher)
+	Name = models.CharField("Name of Class", max_length = 50)
+	Grade = models.ForeignKey(Grade)
+	School = models.ForeignKey(Schools)
+	Teachers = models.ManyToManyField(Teacher)
 		
-		
+	def __unicode__(self): #this names the variable for the admin site
+		Unique_Name = self.Name+self.School
+		return Unique_Name	
 	
