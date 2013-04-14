e3ec0bb1214c0883a09a1a9585bb126b78298d38
Mike_linux <sodium56@gmail.com>
2013-04-13 21:12:25 -0400

    [got foreign key uploading to work,now need to add better comments on teacher creation 2]

--- at_tracker/mysite/AT_Tracker/management/commands/Teacher_Creation2.py
+++ at_tracker/mysite/AT_Tracker/management/commands/Teacher_Creation2.py
@@ -3,7 +3,7 @@ import csv
 from django.core.management.base import BaseCommand
 from django.core.management.base import CommandError
 
-from AT_Tracker.models import teacher
+from AT_Tracker.models import teacher,school
 
 
 class Command(BaseCommand):
@@ -17,13 +17,22 @@ class Command(BaseCommand):
         with open(path) as f:
             reader = csv.reader(f)
             for row in reader:
-                _, created = teacher.objects.get_or_create(
-                    First_Name=row[0],
-                    Last_Name=row[1],
-                    Middle_Name=row[2],
-                    School=row [3]
+				First_Name=row[0],
+				Last_Name=row[1],
+				Middle_Name=row[2]
+				School_Name=row[3]
+				School_Location=row[4]
+				query_1=school.objects.get(Name__iexact=School_Name,City__iexact=School_Location)
+				#find the object in the database
+				pk_q1= query_1.id
+				print pk_q1
+				_, created = teacher.objects.get_or_create(
+					First_Name=First_Name,
+					Last_Name=Last_Name,
+					Middle_Name=Middle_Name,
+					School=pk_q1
                     )
                 # creates a tuple of the new object or
                 # current object and a boolean of if it was created
-                if not created:
-                    self.stderr.write('The Teacher "%s,%s,%s" already exists.' % (row[0],row[1],row[2]))
+				if not created:
+					self.stderr.write('The Teacher "%s,%s,%s" already exists.' % (row[0],row[1],row[2]))
